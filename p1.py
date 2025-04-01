e7c8dabb0e9e0a44d9fb4d914937007293baa971
Koteshwar Rao Darapureddy <116370494+koti19@users.noreply.github.com>
2025-04-01 16:36:49 +0530

    Add files via upload

---
+++ p1.py
@@ -0,0 +1,25 @@
+import random
+
+target = random.randint(1, 100)
+
+while True:
+    yourchoice = input("Guess the target or Quit(Q): ")
+
+    if(yourchoice == "Q"):
+        break
+        
+    yourchoice = int(yourchoice)
+
+    
+    if(yourchoice == target):
+        print("CORRECT GUESS!!!")
+        break
+    elif(yourchoice < target):
+        print("Your number is small. Take a bigger guess...")
+    else:
+        print("Your number is big. Take a smaller guess...")
+    
+
+
+
+print("-----GAME OVER!!!-----")
